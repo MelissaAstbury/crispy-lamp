68cf1d89fb3a7bf3d700561d30eba5d4c88257d8
Melissa Astbury <melissajastbury@gmail.com>
2020-05-13 10:18:50 +0100

    installed nodemon and express to help set up my backend. Also tested this with a Route to know the server is listening

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+const express = require("express");
+
+const app = express();
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("we are on home");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("We are on posts");
+});
+
+//How to listen to the server
+app.listen(8000);
+
 const config = require("./config");
 
-mongoose.set("useCreateIndex", true);
-mongoose
-  .connect(
-    `mongodb+srv://${config.user}:${config.password}@cluster-fuqu4.mongodb.net/crispy_lamp?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Connection Failed!", err);
-  });
+// mongoose.set("useCreateIndex", true);
+// mongoose
+//   .connect(
+//     `mongodb+srv://${config.user}:${config.password}@cluster-fuqu4.mongodb.net/crispy_lamp?retryWrites=true&w=majority`,
+//     { useNewUrlParser: true, useUnifiedTopology: true }
+//   )
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch((err) => {
+//     console.log("Connection Failed!", err);
+//   });
